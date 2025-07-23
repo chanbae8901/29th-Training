7ff4adb3966547cf1a6b3730cdbce41c2f46bc78
cbaeucsd <102708712+cbaeucsd@users.noreply.github.com>
2025-07-22 19:57:58 -0700

    Moved now unused functions to archives
    Commented out moved functions in cfgFunctions.hpp
    
    Helps organization and possible future complete removal.

--- functions/29th_Training/CfgFunctions.hpp
+++ functions/29th_Training/CfgFunctions.hpp
@@ -2,16 +2,16 @@ class 29th_Training {
   tag = "Hill";
   class 29th {
     file = "functions\29th_Training";
-    class removeAllRespawnInventory {};
     class spectator {};
     class cleaner {};
     class setInsignia {};
     class handleInitialInventory {};
     class noThermals {};
-    class assignCurator {};
     class addRadio {};
-    //class randomizeRadioHz {};
     class arsenalClosed {};
     class removeRadio {};
+    //class assignCurator {};
+    //class randomizeRadioHz {};
+    //class removeAllRespawnInventory {};    
   };
 };
