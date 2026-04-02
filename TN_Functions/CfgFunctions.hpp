18c0ced153e4cabce30161ba7882dcb7d7f74c3e
chanbae8901 <102708712+chanbae8901@users.noreply.github.com>
2026-04-02 05:25:54 -0700

    CfgFunctions in Alphabetical Order

--- TN_Functions/CfgFunctions.hpp
+++ TN_Functions/CfgFunctions.hpp
@@ -1,12 +1,21 @@
 //no #include defines needed, inherited from description.ext
-class TN_Loadout {
-    tag = "TN_loadout";
-    class LoadoutFunctions {
-        file = "TN_Functions\loadout";
+class TN_Base {
+    tag = "TN_base";
+    class BaseFunctions {
+        file = "TN_Functions\base";
+        class arsenalZoneCheck {};
+        class cleaner {};
         class init {};
-        class fullSetUnitLoadout {};
-        class onArsenalClosed {};
-        class flexibleReset {};
+    };
+};
+
+class TN_Commands {
+    tag = "TN_commands";
+    class CommandFunctions {
+        file = "TN_Functions\commands";
+        class addModule {};
+        class execute {};
+        class init {};
     };
 };
 
@@ -14,11 +23,11 @@ class TN_Common {
     tag = "TN_common";
     class CommonFunctions {
         file = "TN_Functions\common";
-        class init {};
         class addDiaryRecord {};
+        class convertSide {};
         class diag_log {};
         class displayMsg {};
-        class convertSide {};
+        class init {};
         class notifyAdmin {};
     };
     class InitFunctions {
@@ -28,106 +37,43 @@ class TN_Common {
     };
 };
 
-class TN_Round {
-    tag = "TN_round";
-    class RoundFunctions {
-        file = "TN_Functions\round";
-        class init {};
-        class manageReady {};
-        class checkAllSidesReady {};
-        class initSafeStart {};
-        class initSafeStartHelper {};
-        class changeForcedSafeStart {};
-        class start {};
-        class end {};
-        class addTime {};
-        class getTime {};
-        class setTimer {};
-        class setOvertimeEnabled {};
-        class setOverTimePeriod {};
-        class timeWarning {};
-        class roundEvents {};
-        class formatTime {};
-        class collectSilentWeapons {};
-        class initReadyUI {};
-    };
-    class ReadyUI {
-        file = "TN_Functions\round\readyui";
-        class createReadyUIControls {};
-        class stopReadyUIPFH {};
-        class updateReadyUI {};
-        class flashReadyUI {};
-        class handleReadyChange {};
-    };
-};
-
-class TN_Radio {
-    tag = "TN_radio";
-    class RadioFunctions {
-        file = "TN_Functions\radio";
-        class init {};
-        class initTransferSettings {};
-        class add {};
-        class remove {};
-        class handleFrequencyChanged {};
-        class handleLoadoutLr {};
-    };
-};
-
-class TN_Thermals {
-    tag = "TN_thermals";
-    class ThermalFunctions {
-        file = "TN_Functions\thermals";
-        class init {};
-        class disablePIP {};
-        class blackScreen {};
-    };
-};
-
-class TN_Ticket {
-    tag = "TN_ticket";
-    class TicketFunctions {
-        file = "TN_Functions\ticket";
-        class init {};
-        class add {};
-        class count {};
-    };
-};
-
-class TN_Spectator {
-    tag = "TN_spectator";
-    class SpectatorFunctions {
-        file = "TN_Functions\spectator";
-        class init {};
-        class enter {};
-        class exit {};
-    };
-};
-
 class TN_Curator {
     tag = "TN_curator";
     class CuratorFunctions {
         file = "TN_Functions\curator";
-        class init {};
-        class excludeObjects {};
         class addEditable {};
         class createModule {};
+        class excludeObjects {};
         class handleAdminStateChanged {};
+        class init {};
     };
 };
 
-class TN_Vehicle {
-    tag = "TN_vehicle";
-    class VehicleFunctions {
-        file = "TN_Functions\vehicle";
+class TN_Event {
+    tag = "TN_event";
+    class EventFunctions {
+        file = "TN_Functions\event";
+        class aliveCheck {};
+        class checkWinCondition {};
+        class flagActions {};
+        class game {};
+        class gui_flagMenu {};
+        class gui_setSafeStartTime {};
+        class handleAdminEventMenu {};
         class init {};
+        class markEditorPlacedObjects {};
+        class respawn {};
     };
+};
 
-    class ACELockFixFunctions {
-        file = "TN_Functions\vehicle\ACELockFix";
-        class lockFixInit {};
-        class saveUnconsciousSeat {};
-        class unlockUnconsciousSeat {};
+class TN_Loadout {
+    tag = "TN_loadout";
+    class LoadoutFunctions {
+        file = "TN_Functions\loadout";
+        class flexibleReset {};
+        class fullSetUnitLoadout {};
+        class init {};
+        class onArsenalClosed {};
     };
 };
 
@@ -140,72 +86,62 @@ class TN_OCAP {
     };
 };
 
-class TN_Commands {
-    tag = "TN_commands";
-    class CommandFunctions {
-        file = "TN_Functions\commands";
+class TN_Parade {
+    tag = "TN_parade";
+    class ParadeFunctions {
+        file = "TN_Functions\parade";
+        class checkNonCombatLoadout {};
+        class forceAll {};
+        class handleInitialInventory {};
         class init {};
-        class execute {};
-        class addModule {};
+        class load {};
+        class setInsignia {};
     };
 };
 
-class TN_Training {
-    tag = "TN_training";
-    class TrainingFunctions {
-        file = "TN_Functions\training";
+class TN_Radio {
+    tag = "TN_radio";
+    class RadioFunctions {
+        file = "TN_Functions\radio";
+        class add {};
+        class handleFrequencyChanged {};
+        class handleLoadoutLr {};
         class init {};
-        class initDateAndWeather {};
-        class initDefaultLoadouts {};
-    };
-    class Override {
-        class overrideFunctions {
-            file = "TN_Functions\training\overrideFunctions.sqf";
-            preInit = 1;
-        };
+        class initTransferSettings {};
+        class remove {};
     };
 };
 
-class TN_Tracker {
-    tag = "TN_tracker";
-    class TrackerFunctions {
-        file = "TN_Functions\tracker";
+class TN_Round {
+    tag = "TN_round";
+    class ReadyUI {
+        file = "TN_Functions\round\readyui";
+        class createReadyUIControls {};
+        class flashReadyUI {};
+        class handleReadyChange {};
+        class stopReadyUIPFH {};
+        class updateReadyUI {};
+    };
+    class RoundFunctions {
+        file = "TN_Functions\round";
+        class addTime {};
+        class changeForcedSafeStart {};
+        class checkAllSidesReady {};
+        class collectSilentWeapons {};
+        class end {};
+        class formatTime {};
+        class getTime {};
         class init {};
-        class recordKill {};
-        class saveEvent {};
-        class recordSectorCapture {};
-        class recordACEConscious {};
-        class getName {};
-        class nameToNum {};
-        class weaponToNum {};
-        class getSideAtTime {};
-        class getWeapon {};
-        class addEventHandlersClient {};
-        class findIncendiaryGrenade {};
-        class findSide {};
-        class sendHit {};
-    };
-    class EventHandlerFunctions {
-        file = "TN_Functions\tracker\eventhandlers";
-        class handleFired {};
-        class handleHit {};
-        class handleVehicleKilled {};
-        class handleExplosivePlace {};
-        class handleWoundReceived {};
-        class handleBurnSimulation {};
-    };
-    class DiaryFunctions {
-        file = "TN_Functions\tracker\diary";
-        class createDiaryEntries {};
-        class eventToString {};
-        class killCountsToString {};
-        class colorNameWithSide {};
-        class findPlayerEvents {};
-        class getKillCounts {};
-        class copyRecordToClipboard {};
-        class copyToClipboard {};
-        class sendAll {};
-        class receiveAll {};
+        class initReadyUI {};
+        class initSafeStart {};
+        class initSafeStartHelper {};
+        class manageReady {};
+        class roundEvents {};
+        class setOvertimeEnabled {};
+        class setOverTimePeriod {};
+        class setTimer {};
+        class start {};
+        class timeWarning {};
     };
 };
 
@@ -218,42 +154,105 @@ class TN_Settings {
     };
 };
 
-class TN_Parade {
-    tag = "TN_parade";
-    class ParadeFunctions {
-        file = "TN_Functions\parade";
+class TN_Spectator {
+    tag = "TN_spectator";
+    class SpectatorFunctions {
+        file = "TN_Functions\spectator";
+        class enter {};
+        class exit {};
         class init {};
-        class handleInitialInventory {};
-        class forceAll {};
-        class load {};
-        class checkNonCombatLoadout {};
-        class setInsignia {};
     };
 };
 
-class TN_Event {
-    tag = "TN_event";
-    class EventFunctions {
-        file = "TN_Functions\event";
+class TN_Thermals {
+    tag = "TN_thermals";
+    class ThermalFunctions {
+        file = "TN_Functions\thermals";
+        class blackScreen {};
+        class disablePIP {};
         class init {};
-        class game {};
-        class aliveCheck {};
-        class respawn {};
-        class flagActions {};
-        class checkWinCondition {};
-        class gui_setSafeStartTime {};
-        class gui_flagMenu {};
-        class markEditorPlacedObjects {};
-        class handleAdminEventMenu {};
     };
 };
 
-class TN_Base {
-    tag = "TN_base";
-    class BaseFunctions {
-        file = "TN_Functions\base";
+class TN_Ticket {
+    tag = "TN_ticket";
+    class TicketFunctions {
+        file = "TN_Functions\ticket";
+        class add {};
+        class count {};
         class init {};
-        class cleaner {};
-        class arsenalZoneCheck {};
+    };
+};
+
+class TN_Tracker {
+    tag = "TN_tracker";
+    class DiaryFunctions {
+        file = "TN_Functions\tracker\diary";
+        class colorNameWithSide {};
+        class copyRecordToClipboard {};
+        class copyToClipboard {};
+        class createDiaryEntries {};
+        class eventToString {};
+        class findPlayerEvents {};
+        class getKillCounts {};
+        class killCountsToString {};
+        class receiveAll {};
+        class sendAll {};
+    };
+    class EventHandlerFunctions {
+        file = "TN_Functions\tracker\eventhandlers";
+        class handleBurnSimulation {};
+        class handleExplosivePlace {};
+        class handleFired {};
+        class handleHit {};
+        class handleVehicleKilled {};
+        class handleWoundReceived {};
+    };
+    class TrackerFunctions {
+        file = "TN_Functions\tracker";
+        class addEventHandlersClient {};
+        class findIncendiaryGrenade {};
+        class findSide {};
+        class getName {};
+        class getSideAtTime {};
+        class getWeapon {};
+        class init {};
+        class nameToNum {};
+        class recordACEConscious {};
+        class recordKill {};
+        class recordSectorCapture {};
+        class saveEvent {};
+        class sendHit {};
+        class weaponToNum {};
+    };
+};
+
+class TN_Training {
+    tag = "TN_training";
+    class Override {
+        class overrideFunctions {
+            file = "TN_Functions\training\overrideFunctions.sqf";
+            preInit = 1;
+        };
+    };
+    class TrainingFunctions {
+        file = "TN_Functions\training";
+        class init {};
+        class initDateAndWeather {};
+        class initDefaultLoadouts {};
+    };
+};
+
+class TN_Vehicle {
+    tag = "TN_vehicle";
+    class ACELockFixFunctions {
+        file = "TN_Functions\vehicle\ACELockFix";
+        class lockFixInit {};
+        class saveUnconsciousSeat {};
+        class unlockUnconsciousSeat {};
+    };
+    class VehicleFunctions {
+        file = "TN_Functions\vehicle";
+        class init {};
     };
 };
